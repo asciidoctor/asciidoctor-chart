c9aaba532ec3ff4c500d6efc6ce8545b713693eb
Guillaume Grossetie <ggrossetie@gmail.com>
2022-01-04 10:14:08 +0100

    resolves #12 replace onload by addEventListener

--- lib/asciidoctor/chart/chartjs/chart_builder.rb
+++ lib/asciidoctor/chart/chartjs/chart_builder.rb
@@ -38,11 +38,11 @@ module Asciidoctor
           <<~EOS
           #{chart_canvas}
           <script>
-          window.onload = function() {
+          window.addEventListener('load', function(event) {
           #{chart_init_ctx_script}
           #{chart_init_data_script}
           #{chart_init_script}
-          }
+          })
           </script>
           EOS
         end
