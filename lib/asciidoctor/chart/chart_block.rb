c1c24ac990b09988b972faeec871380e32174713
Guillaume Grossetie <ggrossetie@gmail.com>
2022-02-25 17:11:21 +0100

    include assets (styles, scripts) when an chart engine is used

--- lib/asciidoctor/chart/chart_block.rb
+++ lib/asciidoctor/chart/chart_block.rb
@@ -4,14 +4,7 @@ module Asciidoctor
   module Chart
     class ChartBlock < Asciidoctor::Block
       def initialize(parent, data, attrs)
-        parent_document = parent.document
-        engine = if attrs.key? 'engine'
-                   attrs['engine'].downcase
-                 elsif parent_document.attributes.key? 'chart-engine'
-                   parent_document.attributes['chart-engine'].downcase
-                 else
-                   'c3js'
-                 end
+        engine = parent.document.get_engine attrs
         caption = attrs.delete 'caption'
         title = attrs.delete 'title'
         block_attributes = attrs.merge({
@@ -23,6 +16,29 @@ module Asciidoctor
         super parent, :chart, { source: nil, attributes: block_attributes, subs: nil }
         @title = title
         assign_caption(caption, 'figure')
+      end
+    end
+
+    module ChartBlockTracker
+      attr_reader :x_chart
+
+      def self.extended instance
+        instance.instance_variable_set :@x_chart, {
+          engines: Set.new
+        }
+      end
+
+      def get_engine attrs
+        engine = if attrs.key? 'engine'
+                   attrs['engine'].downcase
+                 elsif @attributes.key? 'chart-engine'
+                   @attributes['chart-engine'].downcase
+                 else
+                   'c3js'
+                 end
+        @x_chart[:engines].add(engine)
+
+        engine
       end
     end
   end
---
+++ spec/docinfo_processor_spec.rb
@@ -0,0 +1,151 @@
+# frozen_string_literal: true
+
+require 'asciidoctor'
+require_relative 'spec_helper'
+require_relative '../lib/asciidoctor-chart'
+
+describe 'Asciidoctor::Chart::DocinfoProcessor' do
+  it 'should append only the required styles and scripts (C3.js)' do
+    input = <<~'ADOC'
+      [chart#month-stats,line]
+      ....
+      January,February,March,April,May,June,July
+      28,48,40,19,86,27,90
+      65,59,80,81,56,55,40
+      ....
+    ADOC
+    output = Asciidoctor.convert(input, standalone: true)
+    (expect output.strip).to include %(<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.3.0/c3.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.3.0/c3.min.js"></script>
+<style>
+  .chartblock {
+    margin-bottom: 1.25em;
+  }
+  .chartblock > .title {
+    text-rendering: optimizeLegibility;
+    text-align: left;
+    font-size: 1rem;
+    font-style: italic;
+    line-height: 1.45;
+    color: #7a2518;
+    font-weight: 400;
+  }
+  .chartblock > .chartjs-content {
+    position: relative;
+    margin-bottom: 0.25em;
+  }
+</style>)
+    (expect output.strip).not_to include %(<script src="https://cdn.jsdelivr.net/chartist.js/)
+    (expect output.strip).not_to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/)
+  end
+  it 'should append only the required styles and scripts (chartist.js)' do
+    input = <<~'ADOC'
+      :chart-engine: chartist
+
+      [chart#month-stats,line]
+      ....
+      January,February,March,April,May,June,July
+      28,48,40,19,86,27,90
+      65,59,80,81,56,55,40
+      ....
+    ADOC
+    output = Asciidoctor.convert(input, standalone: true)
+    (expect output.strip).to include %(<link rel="stylesheet" href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+<script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+<style>
+  .chartblock {
+    margin-bottom: 1.25em;
+  }
+  .chartblock > .title {
+    text-rendering: optimizeLegibility;
+    text-align: left;
+    font-size: 1rem;
+    font-style: italic;
+    line-height: 1.45;
+    color: #7a2518;
+    font-weight: 400;
+  }
+  .chartblock > .chartjs-content {
+    position: relative;
+    margin-bottom: 0.25em;
+  }
+</style>)
+    (expect output.strip).not_to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/)
+    (expect output.strip).not_to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/)
+    (expect output.strip).not_to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/)
+  end
+  it 'should append only the required styles and scripts (Chart.js)' do
+    input = <<~'ADOC'
+      :chart-engine: chartjs
+
+      [chart#month-stats,line]
+      ....
+      January,February,March,April,May,June,July
+      28,48,40,19,86,27,90
+      65,59,80,81,56,55,40
+      ....
+    ADOC
+    output = Asciidoctor.convert(input, standalone: true)
+    (expect output.strip).to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+<style>
+  .chartblock {
+    margin-bottom: 1.25em;
+  }
+  .chartblock > .title {
+    text-rendering: optimizeLegibility;
+    text-align: left;
+    font-size: 1rem;
+    font-style: italic;
+    line-height: 1.45;
+    color: #7a2518;
+    font-weight: 400;
+  }
+  .chartblock > .chartjs-content {
+    position: relative;
+    margin-bottom: 0.25em;
+  }
+</style>)
+    (expect output.strip).not_to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/)
+    (expect output.strip).not_to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/)
+    (expect output.strip).not_to include %(<script src="https://cdn.jsdelivr.net/chartist.js/)
+  end
+  it 'should append only the required styles and scripts (Chart.js + C3.js)' do
+    input = <<~ADOC
+      :chart-engine: chartjs
+
+      [chart#month-stats,line]
+      ....
+      January,February,March,April,May,June,July
+      28,48,40,19,86,27,90
+      65,59,80,81,56,55,40
+      ....
+
+      chart::#{fixturedir}/sample-data.csv[line,engine=c3js]
+    ADOC
+    output = Asciidoctor.convert(input, standalone: true)
+    (expect output.strip).to include %(<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.3.0/c3.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.3.0/c3.min.js"></script>
+<style>
+  .chartblock {
+    margin-bottom: 1.25em;
+  }
+  .chartblock > .title {
+    text-rendering: optimizeLegibility;
+    text-align: left;
+    font-size: 1rem;
+    font-style: italic;
+    line-height: 1.45;
+    color: #7a2518;
+    font-weight: 400;
+  }
+  .chartblock > .chartjs-content {
+    position: relative;
+    margin-bottom: 0.25em;
+  }
+</style>)
+    (expect output.strip).not_to include %(<script src="https://cdn.jsdelivr.net/chartist.js/)
+  end
+end
