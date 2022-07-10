ae6288eb675a65e42a82281b0d470fa201114ee1
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-07-10 14:56:12 +0200

    remove comment

--- cypress.config.js
+++ cypress.config.js
@@ -8,8 +8,6 @@ module.exports = defineConfig({
     failSilently: false,
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
